871d6c5103a9ae3009dc5818f7a90a8f5f0d4851
David <dcicero493@gmail.com>
2020-07-06 14:23:31 -0300

    Estrutura condicional else if

---
+++ EstruturaCondicional/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("[ + ] Somar | [ * ] Multiplicar");
+            string opcao = Console.ReadLine();
+
+            Console.Clear();
+
+            if(opcao == "+")
+            {
+                Console.WriteLine("Resultado da Soma: " + (numero1 + numero2));
+            }else if (opcao == "*")            
+            {
+                Console.WriteLine("Resultado da Multiplicação: " + (numero1 * numero2));
+            }
+            else
+            {
+                Console.WriteLine("Operador Inválido!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
